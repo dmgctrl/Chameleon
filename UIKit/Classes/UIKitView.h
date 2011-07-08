dmgctrl/Chameleon

commit 0c3134db4ac89e274fd18c83c5bfff2496588514

Setup UIKitView to be the delegate for NSWindow
There was an issue with the NSWindow losing key and when -makeKeyAndOrderFront was being called on the
NSWindow the UIWindow was not regaining key status.

UIKit/Classes/UIKitView.h
@@ -32,7 +32,7 @@
 
 @class UIScreen, UIWindow;
 
-@interface UIKitView : NSView {
+@interface UIKitView : NSView <NSWindowDelegate> {
     UIScreen *_screen;
     UIWindow *_mainWindow;
     NSTrackingArea *_trackingArea;

UIKit/Classes/UIKitView.m
@@ -95,6 +95,7 @@
 		[[self layer] insertSublayer:[_screen _layer] atIndex:0];
 		[_screen _layer].frame = [self layer].bounds;
 		[_screen _layer].autoresizingMask = kCALayerWidthSizable | kCALayerHeightSizable;
+        self.window.delegate = self;
 	}
 }
 
@@ -290,6 +291,11 @@
     } else {
         [self _launchApplicationDelegate:appDelegate];
     }
+}
+
+- (void) windowDidBecomeKey:(NSNotification*)notification
+{
+    [_mainWindow makeKeyAndVisible];
 }
 
 @end

commit 0d1a5415df0755c32e76a8ddcdd676719ba5262e

Fixed Bug with introduced in commit 0c3134db4
Should not be setting the UIKitView as the window delegate. Changed this
to a notification.

UIKit/Classes/UIKitView.h
@@ -32,7 +32,7 @@
 
 @class UIScreen, UIWindow;
 
-@interface UIKitView : NSView <NSWindowDelegate> {
+@interface UIKitView : NSView {
     UIScreen *_screen;
     UIWindow *_mainWindow;
     NSTrackingArea *_trackingArea;

UIKit/Classes/UIKitView.m
@@ -35,7 +35,9 @@
 #import "UIImageView.h"
 #import "UIColor.h"
 
-@implementation UIKitView
+@implementation UIKitView {
+    id _didBecomeKeyObserver;
+}
 @synthesize UIScreen=_screen;
 
 - (void)configureLayers
@@ -91,12 +93,18 @@
 }
 
 - (void)viewDidMoveToWindow {
+    [_didBecomeKeyObserver release];
 	if(self.window != nil) {
 		[[self layer] insertSublayer:[_screen _layer] atIndex:0];
 		[_screen _layer].frame = [self layer].bounds;
 		[_screen _layer].autoresizingMask = kCALayerWidthSizable | kCALayerHeightSizable;
-        self.window.delegate = self;
-	}
+
+        _didBecomeKeyObserver = [[NSNotificationCenter defaultCenter] addObserverForName:NSWindowDidBecomeKeyNotification object:self.window queue:nil usingBlock:^(NSNotification* notification) {
+            [_mainWindow makeKeyAndVisible];
+        }];
+	} else {
+        _didBecomeKeyObserver = nil;
+    }
 }
 
 - (BOOL)acceptsFirstResponder
@@ -291,11 +299,6 @@
     } else {
         [self _launchApplicationDelegate:appDelegate];
     }
-}
-
-- (void) windowDidBecomeKey:(NSNotification*)notification
-{
-    [_mainWindow makeKeyAndVisible];
 }
 
 @end