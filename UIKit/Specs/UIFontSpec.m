4c0c5904a3e3da0185ea652dc285edce4c74beaa
Jason Allum <jason@dmgctrl.com>
2013-06-18 18:47:39 -0400

    Added skeletal UIFont spec.

---
+++ UIKit/Specs/UIFontSpec.m
@@ -0,0 +1,17 @@
+SPEC_BEGIN(UIFontSpec)
+
+describe(@"UIFont", ^{
+    context(@"+sytemFontOfSize:", ^{
+        context(@"when called with 17.0", ^{
+            UIFont* font1 = [UIFont systemFontOfSize:17.0];
+            context(@"called with 17.0 a second time", ^{
+                UIFont* font2 = [UIFont systemFontOfSize:17.0];
+                it(@"returns the same font", ^{
+                    [[font1 should] equal:font2];
+                });
+            });
+        });
+    });
+});
+
+SPEC_END
