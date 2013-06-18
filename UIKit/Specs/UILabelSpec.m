78a4f2b1f75893959d6f2205c339a0e79819d033
Jason Allum <jason@dmgctrl.com>
2013-06-18 18:48:16 -0400

    Improved UILabel spec.

--- UIKit/Specs/UILabelSpec.m
+++ UIKit/Specs/UILabelSpec.m
@@ -1,6 +1,25 @@
 SPEC_BEGIN(UILabelSpec)
 
 describe(@"UILabel", ^{
+    context(@"when using the default constructor", ^{
+        UILabel* label = [[UILabel alloc] init];
+        
+        context(@"the text property", ^{
+            NSString* text = [label text];
+            
+            it(@"is empty", ^{
+                [[text should] equal:@""];
+            });
+        });
+        
+        context(@"the font property", ^{
+            UIFont* font = [label font];
+            
+            it(@"is system-font of size 17.0", ^{
+                [[font should] equal:[UIFont systemFontOfSize:17.0f]];
+            });
+        });
+    });
 });
 
 SPEC_END
