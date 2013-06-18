a7a4aca95616983047338b4a3f935cecd5befc7a
Jason Allum <jason@dmgctrl.com>
2013-06-18 19:55:18 -0400

    Added specs.

--- UIKit/Specs/UILabelSpec.m
+++ UIKit/Specs/UILabelSpec.m
@@ -4,19 +4,35 @@ describe(@"UILabel", ^{
     context(@"when using the default constructor", ^{
         UILabel* label = [[UILabel alloc] init];
         
-        context(@"the text property", ^{
+        context(@"text", ^{
             NSString* text = [label text];
             
             it(@"is empty", ^{
                 [[text should] equal:@""];
             });
         });
-        
-        context(@"the font property", ^{
+
+        context(@"attributedText", ^{
+            NSAttributedString* attributedText = [label attributedText];
+            
+            it(@"is empty", ^{
+                [[attributedText should] equal:[[NSAttributedString alloc] initWithString:@""]];
+            });
+        });
+
+        context(@"font", ^{
             UIFont* font = [label font];
             
             it(@"is system-font of size 17.0", ^{
                 [[font should] equal:[UIFont systemFontOfSize:17.0f]];
+            });
+        });
+        
+        context(@"textColor", ^{
+            UIColor* textColor = [label textColor];
+            
+            it(@"is black", ^{
+                [[textColor should] equal:[UIColor blackColor]];
             });
         });
     });
