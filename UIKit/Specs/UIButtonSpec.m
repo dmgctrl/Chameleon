bcc1465e5630cebb83e071a91fb4c4b435e511db
jruyhm <jruhym@gmail.com>
2013-06-25 18:42:46 -0400

    added two button tests

--- UIKit/Specs/UIButtonSpec.m
+++ UIKit/Specs/UIButtonSpec.m
@@ -1,12 +1,24 @@
 SPEC_BEGIN(UIButtonSpec)
-
 describe(@"UIButton", ^{
-
-    context(@"", ^{
+    
+    __block UIButton* button;
+    NSString* text = @"The quick brown fox\n jumped over the lazy \ndog.";
+    NSAttributedString* attributedText = [[NSAttributedString alloc] initWithString:text];
+    
+    context(@"Plain Text title", ^{
         
-        it(@"", ^{
-            
+        beforeEach(^{
+            button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+            [button setTitle:text forState:UIControlStateNormal];
         });
+        
+        it(@"should have one line", ^{
+            [[@([[button titleLabel] numberOfLines]) should] equal:@(1)];
+        });
+        it(@"should have middle trucation as linebreak mode", ^{
+            [[[[button titleLabel] lineBreakMode] == UILineBreakModeTailTruncation should] beYes];
+        });
+        
     });
 });
 
