ade1df7fadf6fea2a744218b817f633301adc1d3
jruyhm <jruhym@gmail.com>
2013-06-27 15:40:04 -0400

    added the remaining tests for the default button properties

--- UIKit/Specs/UIButtonSpec.m
+++ UIKit/Specs/UIButtonSpec.m
@@ -4,17 +4,120 @@
 SPEC_BEGIN(UIButtonSpec)
 describe(@"UIButton", ^{
 
-    context(@"with the default constructor", ^{
+    context(@"default property", ^{
         UIButton* button = [[UIButton alloc] init];
-        it(@"should have one line", ^{
-            [[@([[button titleLabel] numberOfLines]) should] equal:@(1)];
+        
+        context(@"titleLabel", ^{
+            it(@"should have 1 line", ^{
+                [[@([[button titleLabel] numberOfLines]) should] equal:@(1)];
+            });
+            it(@"should be middle trucated", ^{
+                [[@([[button titleLabel] lineBreakMode]) should] equal:@(UILineBreakModeMiddleTruncation)];
+            });
         });
-        it(@"should have middle trucation as linebreak mode", ^{
-            [[@([[button titleLabel] lineBreakMode]) should] equal:@(UILineBreakModeMiddleTruncation)];
+        
+        context(@"reversesTitleShadowWhenHighlighted", ^{
+            it(@"should be No", ^{
+                [[@([button reversesTitleShadowWhenHighlighted]) should] beNo];
+            });
         });
-        it(@"has the proper frame", ^{
-            [[NSStringFromCGRect([button frame]) should] equal:NSStringFromCGRect(CGRectMake(0, 0, 0, 0))];
+
+        context(@"adjustsImageWhenHighlighted", ^{
+            it(@"should be Yes", ^{
+                [[@([button adjustsImageWhenHighlighted]) should] beYes];
+            });
         });
+        
+        context(@"adjustsImageWhenDisabled", ^{
+            it(@"should be Yes", ^{
+                [[@([button adjustsImageWhenDisabled]) should] beYes];
+            });
+        });
+        
+        context(@"showsTouchWhenHighlighted", ^{
+            it(@"should be No", ^{
+                [[@([button showsTouchWhenHighlighted]) should] beNo];
+            });
+        });
+        
+        context(@"tintColor", ^{
+            it(@"should be Nil", ^{
+                [[[button tintColor] should] beNil];
+            });
+        });
+        
+        context(@"insets", ^{
+            context(@"contentEdge", ^{
+                it(@"top should be 0", ^{
+                    [[@(UIEdgeInsetsEqualToEdgeInsets([button contentEdgeInsets], UIEdgeInsetsZero)) should] beYes];
+                });
+            });
+            context(@"titleEdge", ^{
+                it(@"should be 0s", ^{
+                    [[@(UIEdgeInsetsEqualToEdgeInsets([button titleEdgeInsets], UIEdgeInsetsZero)) should] beYes];
+                });
+            });
+            context(@"imageEdge", ^{
+                it(@"should be 0s", ^{
+                    [[@(UIEdgeInsetsEqualToEdgeInsets([button imageEdgeInsets], UIEdgeInsetsZero)) should] beYes];
+                });
+            });
+        });
+        
+        context(@"frame", ^{
+            it(@"should be 0", ^{
+                [[@(CGRectEqualToRect([button frame], CGRectZero)) should] beYes];
+            });
+        });
+        
+        context(@"buttonType", ^{
+            it(@"should be custom", ^{
+                [[@([button buttonType]) should] equal:@(UIButtonTypeCustom)];
+            });
+        });
+        
+        context(@"currentTitle", ^{
+            it(@"should be blank", ^{
+                [[[button currentTitle] should] beNil];
+            });
+        });
+        
+        context(@"currentAttributedTitle", ^{
+            it(@"should be blank", ^{
+                [[[button currentAttributedTitle] should] beNil];
+            });
+        });
+        
+        context(@"currentTitleColor", ^{
+            it(@"should be white", ^{
+                [[[button currentTitleColor] should] equal:[UIColor whiteColor]];
+            });
+        });
+
+        context(@"currentTitleShadowColor", ^{
+            it(@"should be white[sic]", ^{//Documentation claims default is white
+                [[[button currentTitleShadowColor] should] equal:[[UIColor blackColor] colorWithAlphaComponent:0.5]];
+            });
+        });
+
+        context(@"currentImage", ^{
+            it(@"should be blank", ^{
+                [[[button currentImage] should] beNil];
+            });
+        });
+        
+        context(@"currentBackgroundImage", ^{
+            it(@"should be blank", ^{
+                [[[button currentBackgroundImage] should] beNil];
+            });
+        });
+        
+        context(@"imageView", ^{
+            it(@"should contain no image", ^{
+                [[[[button imageView] image] should] beNil];
+            });
+        });
+
     });
 
     context(@"with a plain text title", ^{
