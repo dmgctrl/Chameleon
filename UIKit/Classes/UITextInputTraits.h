8182a2146409a9e18fc191069133f8420abcb3eb
Jason Allum <jason@dmgctrl.com>
2013-07-15 11:12:24 -0400

    Switched to NS_ENUM

--- UIKit/Classes/UITextInputTraits.h
+++ UIKit/Classes/UITextInputTraits.h
@@ -29,25 +29,25 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum {
+typedef NS_ENUM(NSInteger, UITextAutocapitalizationType) {
     UITextAutocapitalizationTypeNone,
     UITextAutocapitalizationTypeWords,
     UITextAutocapitalizationTypeSentences,
     UITextAutocapitalizationTypeAllCharacters,
-} UITextAutocapitalizationType;
+};
 
-typedef enum {
+typedef NS_ENUM(NSInteger, UITextAutocorrectionType) {
     UITextAutocorrectionTypeDefault,
     UITextAutocorrectionTypeNo,
     UITextAutocorrectionTypeYes,
-} UITextAutocorrectionType;
+};
 
-typedef enum {
+typedef NS_ENUM(NSInteger, UIKeyboardAppearance) {
     UIKeyboardAppearanceDefault,
     UIKeyboardAppearanceAlert,
-} UIKeyboardAppearance;
+};
 
-typedef enum {
+typedef NS_ENUM(NSInteger, UIKeyboardType) {
     UIKeyboardTypeDefault,
     UIKeyboardTypeASCIICapable,
     UIKeyboardTypeNumbersAndPunctuation,
@@ -57,9 +57,9 @@ typedef enum {
     UIKeyboardTypeNamePhonePad,
     UIKeyboardTypeEmailAddress,
     UIKeyboardTypeAlphabet = UIKeyboardTypeASCIICapable
-} UIKeyboardType;
+};
 
-typedef enum {
+typedef NS_ENUM(NSInteger, UIReturnKeyType) {
     UIReturnKeyDefault,
     UIReturnKeyGo,
     UIReturnKeyGoogle,
@@ -71,7 +71,7 @@ typedef enum {
     UIReturnKeyYahoo,
     UIReturnKeyDone,
     UIReturnKeyEmergencyCall,
-} UIReturnKeyType;
+};
 
 @protocol UITextInputTraits <NSObject>
 @property (nonatomic) UITextAutocapitalizationType autocapitalizationType;
