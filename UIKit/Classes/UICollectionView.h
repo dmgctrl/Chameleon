b4dfeab8008110080e9a8fab1c42a4aa5ac1734e
jruyhm <jruhym@gmail.com>
2013-07-16 12:12:32 -0400

    Renamed viewcontrollerspec

---
+++ UIKit/Classes/UICollectionView.h
@@ -0,0 +1,13 @@
+//
+//  UICollectionView.h
+//  UIKit
+//
+//  Created by Andrew Heim on 7/16/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICollectionView : UIScrollView
+
+@end
---
+++ UIKit/Specs/UIControlSpec.m
@@ -0,0 +1,13 @@
+//
+//  UIControllerSpec.m
+//  UIKit
+//
+//  Created by Andrew Heim on 7/16/13.
+//
+//
+
+#import "UIControllerSpec.h"
+
+@implementation UIControllerSpec
+
+@end
