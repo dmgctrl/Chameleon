d42f972014005535abd6d35d1eef3a7379feb064
jruyhm <jruhym@gmail.com>
2013-07-16 15:10:02 -0400

    formatting

--- UIKit/Classes/UICollectionViewDelegate.h
+++ UIKit/Classes/UICollectionViewDelegate.h
@@ -1,5 +1,4 @@
 #import <Foundation/Foundation.h>
-//#import "UICollectionView.h"
 @class UICollectionView;
 @class UICollectionViewCell;
 @class UICollectionReusableView;
@@ -7,26 +6,26 @@
 @protocol UICollectionViewDelegate <NSObject>
 #pragma mark Managing the Selected Cells
 
-- (BOOL) collectionView:(UICollectionView*)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath;
-- (void) collectionView:(UICollectionView*)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath;
-- (BOOL) collectionView:(UICollectionView*)collectionView shouldDeselectItemAtIndexPath:(NSIndexPath *)indexPath;
-- (void) collectionView:(UICollectionView*)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath;
+- (BOOL) collectionView:(UICollectionView*)collectionView shouldSelectItemAtIndexPath:(NSIndexPath*)indexPath;
+- (void) collectionView:(UICollectionView*)collectionView didSelectItemAtIndexPath:(NSIndexPath*)indexPath;
+- (BOOL) collectionView:(UICollectionView*)collectionView shouldDeselectItemAtIndexPath:(NSIndexPath*)indexPath;
+- (void) collectionView:(UICollectionView*)collectionView didDeselectItemAtIndexPath:(NSIndexPath*)indexPath;
 
 #pragma mark Managing Cell Highlighting
 
-- (BOOL) collectionView:(UICollectionView*)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath;
-- (void) collectionView:(UICollectionView*)collectionView didHighlightItemAtIndexPath:(NSIndexPath *)indexPath;
-- (void) collectionView:(UICollectionView*)collectionView didUnhighlightItemAtIndexPath:(NSIndexPath *)indexPath;
+- (BOOL) collectionView:(UICollectionView*)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath*)indexPath;
+- (void) collectionView:(UICollectionView*)collectionView didHighlightItemAtIndexPath:(NSIndexPath*)indexPath;
+- (void) collectionView:(UICollectionView*)collectionView didUnhighlightItemAtIndexPath:(NSIndexPath*)indexPath;
 
 #pragma mark Tracking the Removal of Views
 
-- (void) collectionView:(UICollectionView*)collectionView didEndDisplayingCell:(UICollectionViewCell*)cell forItemAtIndexPath:(NSIndexPath *)indexPath;
-- (void) collectionView:(UICollectionView*)collectionView didEndDisplayingSupplementaryView:(UICollectionReusableView*)view forElementOfKind:(NSString *)elementKind atIndexPath:(NSIndexPath *)indexPath;
+- (void) collectionView:(UICollectionView*)collectionView didEndDisplayingCell:(UICollectionViewCell*)cell forItemAtIndexPath:(NSIndexPath*)indexPath;
+- (void) collectionView:(UICollectionView*)collectionView didEndDisplayingSupplementaryView:(UICollectionReusableView*)view forElementOfKind:(NSString*)elementKind atIndexPath:(NSIndexPath *)indexPath;
 
 #pragma mark Managing Actions for Cells
 
-- (BOOL) collectionView:(UICollectionView*)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath;
-- (BOOL) collectionView:(UICollectionView*)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender;
-- (void) collectionView:(UICollectionView*)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender;
+- (BOOL) collectionView:(UICollectionView*)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath*)indexPath;
+- (BOOL) collectionView:(UICollectionView*)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath*)indexPath withSender:(id)sender;
+- (void) collectionView:(UICollectionView*)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath*)indexPath withSender:(id)sender;
 
 @end
