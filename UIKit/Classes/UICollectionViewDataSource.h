e7847c267ec1a77aa0c89c4a74b948e3361f1d9c
jruyhm <jruhym@gmail.com>
2013-07-16 14:28:48 -0400

    uicollectionviewdatasource

--- UIKit/Classes/UICollectionViewDataSource.h
+++ UIKit/Classes/UICollectionViewDataSource.h
@@ -1,13 +1,13 @@
-//
-//  UICollectionViewDataSource.h
-//  UIKit
-//
-//  Created by Andrew Heim on 7/16/13.
-//
-//
-
 #import <Foundation/Foundation.h>
 
 @protocol UICollectionViewDataSource <NSObject>
+#pragma mark Getting Item and Section Metrics
 
+- (NSInteger)collectionView:(UICollectionView*)collectionView numberOfItemsInSection:(NSInteger)section;
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)collectionView;
+
+#pragma mark Getting Views for Items
+
+- (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView cellForItemAtIndexPath:(NSIndexPath*)indexPath;
+- (UICollectionReusableView*)collectionView:(UICollectionView*)collectionView viewForSupplementaryElementOfKind:(NSString*)kind atIndexPath:(NSIndexPath*)indexPath;
 @end
