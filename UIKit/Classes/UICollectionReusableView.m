0228b6d3520805db3a4fda05f510aa23004a8f4c
jruyhm <jruhym@gmail.com>
2013-07-16 16:44:06 -0400

    UICollectionReusableView

---
+++ UIKit/Classes/UICollectionReusableView.m
@@ -0,0 +1,22 @@
+#import "UICollectionReusableView.h"
+
+@implementation UICollectionReusableView
+- (void)prepareForReuse
+{
+}
+
+#pragma mark Managing Layout Changes
+
+- (void)applyLayoutAttributes:(UICollectionViewLayoutAttributes *)layoutAttributes
+{
+}
+
+- (void)willTransitionFromLayout:(UICollectionViewLayout *)oldLayout toLayout:(UICollectionViewLayout *)newLayout
+{
+}
+
+- (void)didTransitionFromLayout:(UICollectionViewLayout *)oldLayout toLayout:(UICollectionViewLayout *)newLayout
+{
+}
+
+@end
