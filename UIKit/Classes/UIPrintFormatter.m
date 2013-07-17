06d2e8e7cf3e10289af35bf82ddb0252ffe1559d
jruyhm <jruhym@gmail.com>
2013-07-17 10:38:52 -0400

    fill in stubs for printformatter

--- UIKit/Classes/UIPrintFormatter.m
+++ UIKit/Classes/UIPrintFormatter.m
@@ -1,5 +1,19 @@
 #import "UIPrintFormatter.h"
 
 @implementation UIPrintFormatter
+#pragma mark Drawing the Content
+- (void)drawInRect:(CGRect)rect forPageAtIndex:(NSInteger)pageIndex
+{
+}
+
+- (CGRect)rectForPageAtIndex:(NSUInteger)pageIndex // documentation says (NSIndex)pageIndex;
+{
+    return CGRectNull;
+}
+
+#pragma mark Communicating with the Page Renderer
+- (void)removeFromPrintPageRenderer
+{
+}
 
 @end
