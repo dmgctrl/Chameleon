b1dd43c58c19482c3884b15d6e17b1e6317b70f3
jruyhm <jruhym@gmail.com>
2013-07-17 18:36:05 -0400

    clean up collection reusable view

--- UIKit/Classes/UICollectionReusableView.h
+++ UIKit/Classes/UICollectionReusableView.h
@@ -1,13 +1,12 @@
 #import <UIKit/UIKit.h>
 
 @interface UICollectionReusableView : UIView
-#pragma mark Reusing Cells
 
-@property (nonatomic, readonly, copy) NSString *reuseIdentifier;
+#pragma mark Reusing Cells
+@property (nonatomic, readonly, copy) NSString* reuseIdentifier;
 - (void)prepareForReuse;
 
 #pragma mark Managing Layout Changes
-
 - (void) applyLayoutAttributes:(UICollectionViewLayoutAttributes*)layoutAttributes;
 - (void) willTransitionFromLayout:(UICollectionViewLayout*)oldLayout toLayout:(UICollectionViewLayout*)newLayout;
 - (void) didTransitionFromLayout:(UICollectionViewLayout*)oldLayout toLayout:(UICollectionViewLayout*)newLayout;
