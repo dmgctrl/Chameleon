e92554546755a7a231a5c7f550a06b85f01df776
jruyhm <jruhym@gmail.com>
2013-08-23 14:23:39 -0400

    added stubs to UITableViewHeaderFooterView implementation.

--- UIKit/Classes/UITableViewHeaderFooterView.m
+++ UIKit/Classes/UITableViewHeaderFooterView.m
@@ -2,4 +2,31 @@
 
 @implementation UITableViewHeaderFooterView
 
+#pragma mark Initializing the View
+
+- (instancetype) initWithReuseIdentifier:(NSString*)reuseIdentifier
+{
+#warning implement -initWithReuseIdentifier:
+    UIKIT_STUB_W_RETURN(@"-initWithReuseIdentifier:");
+}
+
+- (void) prepareForReuse
+{
+#warning implement -prepareForReuse
+    UIKIT_STUB(@"-prepareForReuse");
+}
+
+# pragma mark UIApperance Protocol
+
++ (id) appearance
+{
+#warning implement -appearance
+    UIKIT_STUB_W_RETURN(@"-appearance");
+}
+
++ (id) appearanceWhenContainedIn:(Class <UIAppearanceContainer>)ContainerClass, ... NS_REQUIRES_NIL_TERMINATION
+{
+#warning implement -appearanceWhenContainedIn:
+    UIKIT_STUB_W_RETURN(@"-appearanceWhenContainedIn:");
+}
 @end
