aef552b9f55483eaa79815826fbe2d521026129e
jruyhm <jruhym@gmail.com>
2013-08-23 16:32:29 -0400

    formatting

--- UIKit/Classes/UITabBar.h
+++ UIKit/Classes/UITabBar.h
@@ -42,13 +42,11 @@
 @protocol UITabBarDelegate <NSObject>
 @optional
 
-- (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item;
-
-// stub
-- (void)tabBar:(UITabBar *)tabBar willBeginCustomizingItems:(NSArray *)items;                     // called before customize sheet is shown. items is current item list
-- (void)tabBar:(UITabBar *)tabBar didBeginCustomizingItems:(NSArray *)items;                      // called after customize sheet is shown. items is current item list
-- (void)tabBar:(UITabBar *)tabBar willEndCustomizingItems:(NSArray *)items changed:(BOOL)changed; // called before customize sheet is hidden. items is new item list
-- (void)tabBar:(UITabBar *)tabBar didEndCustomizingItems:(NSArray *)items changed:(BOOL)changed;  // called after customize sheet is hidden. items is new item list
+- (void)tabBar:(UITabBar*)tabBar didSelectItem:(UITabBarItem*)item;
+- (void)tabBar:(UITabBar*)tabBar willBeginCustomizingItems:(NSArray*)items;
+- (void)tabBar:(UITabBar*)tabBar didBeginCustomizingItems:(NSArray*)items;
+- (void)tabBar:(UITabBar*)tabBar willEndCustomizingItems:(NSArray*)items changed:(BOOL)changed;
+- (void)tabBar:(UITabBar*)tabBar didEndCustomizingItems:(NSArray*)items changed:(BOOL)changed;
 
 @end
 
@@ -58,7 +56,7 @@
 
 #pragma mark Getting and Setting Properties
 
-@property(nonatomic, assign) id<UITabBarDelegate> delegate;
+@property (nonatomic, assign) id<UITabBarDelegate> delegate;
 
 
 #pragma mark Configuring Items
@@ -70,17 +68,17 @@
 
 #pragma mark Customizing Tab Bars
 
-- (void)beginCustomizingItems:(NSArray *)items;
-- (BOOL)endCustomizingAnimated:(BOOL)animated;
-- (BOOL)isCustomizing;
+- (void) beginCustomizingItems:(NSArray*)items;
+- (BOOL) endCustomizingAnimated:(BOOL)animated;
+- (BOOL) isCustomizing;
 
 
 #pragma mark Customizing Appearance
 
-@property(nonatomic, retain) UIImage *backgroundImage;
-@property(nonatomic, retain) UIColor *selectedImageTintColor;
-@property(nonatomic, retain) UIImage *selectionIndicatorImage;
-@property(nonatomic, retain) UIImage *shadowImage;
-@property(nonatomic, retain) UIColor *tintColor;
+@property (nonatomic, retain) UIImage* backgroundImage;
+@property (nonatomic, retain) UIColor* selectedImageTintColor;
+@property (nonatomic, retain) UIImage* selectionIndicatorImage;
+@property (nonatomic, retain) UIImage* shadowImage;
+@property (nonatomic, retain) UIColor* tintColor;
 
 @end
