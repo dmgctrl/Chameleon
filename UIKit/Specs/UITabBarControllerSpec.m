ad9bacd0365f2c3c0b242be9033c97d981bd998f
jruyhm <jruhym@gmail.com>
2013-08-26 11:51:53 -0400

    Added tests for the values of the properties of a default instance of uitabbarcontroller.

--- UIKit/Specs/UITabBarControllerSpec.m
+++ UIKit/Specs/UITabBarControllerSpec.m
@@ -10,9 +10,34 @@ describe(@"UITabBarController", ^{
         });
         context(@"property", ^{
 
-            context(@"", ^{
-                it(@"should ", ^{
-
+            context(@"tabBar", ^{
+                it(@"should be", ^{
+                    [[[tabBarController tabBar] should] beNonNil];
+                });
+            });
+            context(@"viewControllers", ^{
+                it(@"should be empty", ^{
+                    [[@([[tabBarController viewControllers]count]) should] equal:@(0)];
+                });
+            });
+            context(@"customizableViewControllers", ^{
+                it(@"should be empty", ^{
+                    [[@([[tabBarController customizableViewControllers]count]) should] equal:@(0)];
+                });
+            });
+            context(@"moreNavigationController", ^{
+                it(@"should be", ^{
+                    [[[tabBarController moreNavigationController] should] beNonNil];
+                });
+            });
+            context(@"selectedViewController", ^{
+                it(@"should not be", ^{
+                    [[[tabBarController selectedViewController] should] beNil];
+                });
+            });
+            context(@"selectedIndex", ^{
+                it(@"should be 0", ^{
+                    [[@([tabBarController selectedIndex]) should] equal:@(NSIntegerMax)];
                 });
             });
         });
