37c34d45b0c13b218c3c91211a8337be8c2a6efd
jruyhm <jruhym@gmail.com>
2013-08-26 14:47:06 -0400

    added tests for the values of the properties and instance methods of the default instance of uitabbaritem

--- UIKit/Specs/UITabBarItemSpec.m
+++ UIKit/Specs/UITabBarItemSpec.m
@@ -8,11 +8,29 @@ describe(@"UITabBarItem", ^{
         it(@"should be member of right class", ^{
             [[tabBarItem should] beKindOfClass:[UITabBarItem class]];
         });
+
         context(@"property", ^{
+            context(@"badgeValue", ^{
+                it(@"should not be", ^{
+                    [[[tabBarItem badgeValue] should] beNil];
+                });
+            });
+        });
 
-            context(@"", ^{
-                it(@"should ", ^{
-
+        context(@"instance method", ^{
+            context(@"finishedSelectedImage", ^{
+                it(@"should not be", ^{
+                    [[[tabBarItem finishedSelectedImage] should] beNil];
+                });
+            });
+            context(@"finishedUnselectedImage", ^{
+                it(@"should not be", ^{
+                    [[[tabBarItem finishedUnselectedImage] should] beNil];
+                });
+            });
+            context(@"titlePositionAdjustment", ^{
+                it(@"should not be", ^{
+                    [[@(UIOffsetEqualToOffset(UIOffsetZero, [tabBarItem titlePositionAdjustment])) should] beYes];
                 });
             });
         });
