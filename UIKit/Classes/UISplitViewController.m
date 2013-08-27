0a94b89b92ab73e6650666b0fff9c2b843de56a6
jruyhm <jruhym@gmail.com>
2013-08-27 14:06:09 -0400

    formatting

--- UIKit/Classes/UISplitViewController.m
+++ UIKit/Classes/UISplitViewController.m
@@ -40,20 +40,20 @@ static const CGFloat SplitterPadding = 3;
 
 @interface _UISplitViewControllerView : UIView {
     BOOL dragging;
-    UIView *leftPanel;
-    UIView *rightPanel;
+    UIView* leftPanel;
+    UIView* rightPanel;
 }
 @property (nonatomic, assign) CGFloat leftWidth;
-- (void) addViewControllers:(NSArray *)viewControllers;
+- (void) addViewControllers:(NSArray*)viewControllers;
 @end
 
 @implementation _UISplitViewControllerView
 
-- (id)initWithFrame:(CGRect)frame
+- (id) initWithFrame:(CGRect)frame
 {
     if ((self=[super initWithFrame:frame])) {
-        leftPanel = [(UIView *)[UIView alloc] initWithFrame:CGRectMake(0,0,320,frame.size.height)];
-        rightPanel = [(UIView *)[UIView alloc] initWithFrame:CGRectMake(321,0,MAX(0,frame.size.width-321),frame.size.height)];
+        leftPanel = [(UIView*)[UIView alloc] initWithFrame:CGRectMake(0,0,320,frame.size.height)];
+        rightPanel = [(UIView*)[UIView alloc] initWithFrame:CGRectMake(321,0,MAX(0,frame.size.width-321),frame.size.height)];
         leftPanel.clipsToBounds = rightPanel.clipsToBounds = YES;
         leftPanel.autoresizingMask = UIViewAutoresizingFlexibleHeight;
         rightPanel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
@@ -65,52 +65,45 @@ static const CGFloat SplitterPadding = 3;
     return self;
 }
 
-
-- (void)addViewControllers:(NSArray *)viewControllers
+- (void) addViewControllers:(NSArray*)viewControllers
 {
     if ([viewControllers count] == 2) {
-        UIView *leftView = (UIView*)[[viewControllers objectAtIndex:0] view];
-        UIView *rightView = (UIView*)[[viewControllers objectAtIndex:1] view];
-        
+        UIView* leftView = (UIView*)[[viewControllers objectAtIndex:0] view];
+        UIView* rightView = (UIView*)[[viewControllers objectAtIndex:1] view];
         leftView.autoresizingMask = rightView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-        
         leftView.frame = leftPanel.bounds;
         rightView.frame = rightPanel.bounds;
-        
         [leftPanel addSubview:leftView];
         [rightPanel addSubview:rightView];
     }
 }
 
-- (void)setLeftWidth:(CGFloat)newWidth
+- (void) setLeftWidth:(CGFloat)newWidth
 {
     if (newWidth != leftPanel.frame.size.width) {
         CGRect leftFrame = leftPanel.frame;
         CGRect rightFrame = rightPanel.frame;
         const CGFloat height = self.bounds.size.height;
-        
         leftFrame.origin = CGPointZero;
         leftFrame.size = CGSizeMake(newWidth, height);
-
         rightFrame.origin = CGPointMake(newWidth+1,0);
         rightFrame.size = CGSizeMake(MAX(self.bounds.size.width-newWidth-1,0), height);
-        
         leftPanel.frame = leftFrame;
         rightPanel.frame = rightFrame;
     }
 }
 
-- (CGFloat)leftWidth
+- (CGFloat) leftWidth
 {
     return CGRectGetMaxX(leftPanel.frame);
 }
 
-- (CGRect)splitterHitRect
+- (CGRect) splitterHitRect
 {
     return CGRectMake(self.leftWidth-SplitterPadding,0,SplitterPadding+SplitterPadding+1,self.bounds.size.height);
 }
 
-- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+- (UIView*) hitTest:(CGPoint)point withEvent:(UIEvent*)event
 {
     if (CGRectContainsPoint([self splitterHitRect], point)) {
         return self;
@@ -119,42 +112,38 @@ static const CGFloat SplitterPadding = 3;
     }
 }
 
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+- (void) touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event
 {
     CGPoint point = [[touches anyObject] locationInView:self];
-
     if (CGRectContainsPoint([self splitterHitRect], point)) {
         dragging = YES;
     }
 }
 
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+- (void) touchesMoved:(NSSet*)touches withEvent:(UIEvent*)event
 {
     if (dragging) {
         CGFloat newWidth = [[touches anyObject] locationInView:self].x;
-        
         newWidth = MAX(50, newWidth);
         newWidth = MIN(self.bounds.size.width-50, newWidth);
-        
         self.leftWidth = newWidth;
     }
 }
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+- (void) touchesEnded:(NSSet*)touches withEvent:(UIEvent*)event
 {
     dragging = NO;
 }
 
-- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+- (void) touchesCancelled:(NSSet*)touches withEvent:(UIEvent*)event
 {
     dragging = NO;
 }
 
-- (id)mouseCursorForEvent:(UIEvent *)event
+- (id) mouseCursorForEvent:(UIEvent*)event
 {
     CGRect splitterRect = [self splitterHitRect];
     CGPoint point = [[[event allTouches] anyObject] locationInView:self];
-
     if (dragging && point.x < splitterRect.origin.x) {
         return [NSCursor resizeLeftCursor];
     } else if (dragging && point.x > splitterRect.origin.x+splitterRect.size.width) {
@@ -169,7 +158,6 @@ static const CGFloat SplitterPadding = 3;
 @end
 
 
-
 @implementation UISplitViewController {
     struct {
         BOOL willPresentViewController : 1;
@@ -180,29 +168,29 @@ static const CGFloat SplitterPadding = 3;
 
 #pragma mark Managing the Child View Controllers
 
-- (void) setViewControllers:(NSArray *)newControllers
+- (void) setViewControllers:(NSArray*)newControllers
 {
     assert([newControllers count]==2);
 
     if (![newControllers isEqualToArray:_viewControllers]) {
-        for (UIViewController *c in _viewControllers) {
+        for (UIViewController* c in _viewControllers) {
             [c _setParentViewController:nil];
         }
-        for (UIViewController *c in newControllers) {
+        for (UIViewController* c in newControllers) {
             [c _setParentViewController:self];
         }
 
         if ([self isViewLoaded]) {
-            [(_UISplitViewControllerView *)self.view addViewControllers:_viewControllers];
-            for (UIViewController *c in newControllers) {
+            [(_UISplitViewControllerView*)self.view addViewControllers:_viewControllers];
+            for (UIViewController* c in newControllers) {
                 [c viewWillAppear:NO];
             }
-            for (UIViewController *c in _viewControllers) {
+            for (UIViewController* c in _viewControllers) {
                 if ([c isViewLoaded]) {
                     [c.view removeFromSuperview];
                 }
             }
-            for (UIViewController *c in newControllers) {
+            for (UIViewController* c in newControllers) {
                 [c viewDidAppear:NO];
             }
         }
@@ -213,7 +201,7 @@ static const CGFloat SplitterPadding = 3;
 
 #pragma mark Accessing the Delegate Object
 
-- (void)setDelegate:(id <UISplitViewControllerDelegate>)newDelegate
+- (void) setDelegate:(id <UISplitViewControllerDelegate>)newDelegate
 {
     _delegate = newDelegate;
     _delegateHas.willPresentViewController = [_delegate respondsToSelector:@selector(splitViewController:popoverController:willPresentViewController:)];
@@ -224,48 +212,48 @@ static const CGFloat SplitterPadding = 3;
 
 #pragma mark UIViewController Overrides
 
-- (id)initWithNibName:(NSString *)nibName bundle:(NSBundle *)nibBundle
+- (id) initWithNibName:(NSString*)nibName bundle:(NSBundle*)nibBundle
 {
     if ((self=[super initWithNibName:nibName bundle:nibBundle])) {
     }
     return self;
 }
 
-- (void)loadView
+- (void) loadView
 {
-    self.view = [(_UISplitViewControllerView *)[_UISplitViewControllerView alloc] initWithFrame:CGRectMake(0,0,1024,768)];
+    self.view = [(_UISplitViewControllerView*)[_UISplitViewControllerView alloc] initWithFrame:CGRectMake(0,0,1024,768)];
     self.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 }
 
-- (void)viewWillAppear:(BOOL)animated
+- (void) viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    [(_UISplitViewControllerView *)self.view addViewControllers:_viewControllers];
-    for (UIViewController *c in _viewControllers) {
+    [(_UISplitViewControllerView*)self.view addViewControllers:_viewControllers];
+    for (UIViewController* c in _viewControllers) {
         [c viewWillAppear:animated];
     }
 }
 
-- (void)viewDidAppear:(BOOL)animated
+- (void) viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    for (UIViewController *c in _viewControllers) {
+    for (UIViewController* c in _viewControllers) {
         [c viewDidAppear:animated];
     }
 }
 
-- (void)viewWillDisappear:(BOOL)animated
+- (void) viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
-    for (UIViewController *c in _viewControllers) {
+    for (UIViewController* c in _viewControllers) {
         [c viewWillDisappear:animated];
     }
 }
 
-- (void)viewDidDisappear:(BOOL)animated
+- (void) viewDidDisappear:(BOOL)animated
 {
     [super viewDidDisappear:animated];
-    for (UIViewController *c in _viewControllers) {
+    for (UIViewController* c in _viewControllers) {
         [c viewDidDisappear:animated];
     }
 }
